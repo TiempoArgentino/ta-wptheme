2953d2b2f35af1a019016c013c06f32a1b26c03d
matezito <juan.e@genosha.com.ar>
2021-03-20 20:34:34 -0300

    fix links, subscriptions and newsletter

--- subscriptions-theme/pages/subscriptions-loop.php
+++ subscriptions-theme/pages/subscriptions-loop.php
@@ -61,6 +61,7 @@
                                         ]
                                     );
                                     $query = new WP_Query($args);
+
                                     if ($query->have_posts()) : while ($query->have_posts()) : $query->the_post();
 
 
