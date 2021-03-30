d5383c99d80720063c660b7ebbe38c3b788a5316
matezito <juan.e@genosha.com.ar>
2021-03-30 19:54:51 -0300

    add widget single common note

--- single-ta_article.php
+++ single-ta_article.php
@@ -115,6 +115,15 @@
         </div>
     </div>
 </div>
+<?php if (is_active_sidebar('down-single-note')) { ?>
+    <div class="container d-sm-none d-md-block mt-md-3 mb-md-3">
+        <div class="row d-flex">
+            <div class="col-9 mx-auto">
+                <?php dynamic_sidebar('down-single-note'); ?>
+            </div>
+        </div>
+    </div>
+<?php } ?>
 <?php
 $articles_interest_block = RB_Gutenberg_Block::get_block('ta/articles-interest');
 $articles_interest_block->render(array(
@@ -138,6 +147,32 @@ $articles_interest_block->render(array(
 <div class="ta-context dark-blue-bg">
     <?php include_once(TA_THEME_PATH . '/markup/partes/relacionados-tema.php');  ?>
 </div>
+<div class="container-md">
+    <div class="row">
+        <div class="col-12 col-lg-8">
+            <?php include_once(TA_THEME_PATH . '/markup/partes/comentarios.php');  ?>
+        </div>
+        <div class="col-12 col-lg-4">
+            <?php if (is_active_sidebar('side-comments-note')) { ?>
+                <div class="d-sm-none d-md-block anuncio-side-comments-single">
+                    <?php dynamic_sidebar('side-comments-note'); ?>
+                </div>
+            <?php } ?>
+        </div>
+    </div>
+</div>
+
+<!-- abajo comentarios -->
+<?php if (is_active_sidebar('down-comments-note')) { ?>
+    <div class="container d-sm-none d-md-block mt-md-3 mb-md-3">
+        <div class="row d-flex">
+            <div class="col-9 mx-auto">
+                <?php dynamic_sidebar('down-comments-note'); ?>
+            </div>
+        </div>
+    </div>
+<?php } ?>
+<!-- abajo comentarios -->
 <?php
 $articles_block = RB_Gutenberg_Block::get_block('ta/articles');
 $articles = get_ta_articles_from_query(array(
