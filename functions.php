2cc5a23904a9df3d2e5fe38d57310be65fbce1a1
Juan Cruz Rabaglia <juanrabaa@gmail.com>
2021-06-02 10:23:48 -0300

    .

--- functions.php
+++ functions.php
@@ -270,10 +270,8 @@ class TA_Theme
 		);
 
 		// TODO: Mover esto a su propio method. Controlar valor devuelvto por el WP_Query
-		$most_viewed_query = ta_get_latest_most_viewed_query(array( 'posts_per_page'	=> -1 ));
-
-
 		add_action('wp_footer', function(){
+			$most_viewed_query = ta_get_latest_most_viewed_query(array( 'posts_per_page'	=> -1 ));
 			wp_localize_script(
 				'ta-balancer-front-block-js',
 				'TABalancerApiData',
