418d24f31db1dd3cb4c818671a83281dbd2123d2
Juan Cruz Rabaglia <juanrabaa@gmail.com>
2021-06-10 09:32:57 -0300

    The "Seamos Socios" banner moved into wordpress parts to accept arguments. Arguments changed for the banner shown next to articles grids (sections, tags, etc).

---
+++ parts/common-seamos_socios.php
@@ -0,0 +1,47 @@
+<?php
+/*
+ *
+ */
+$defaults = array(
+    /**
+    *   @param string $title
+    */
+    'title'                     => "En Tiempo Argentino hacemos periodismo sin fines de lucro.",
+    /**
+    *   @param string $body
+    */
+    'body'                      => "<b>Nuestra agenda se guía por los temas socialmente relevantes</span>, por eso abordamos
+                            problemáticas
+                            habitualmente marginadas de los otros medios de comunicación masivos.",
+    /**
+    *   @param string $btn
+    */
+    'btn'                       => "SEAMOS SOCIOS",
+);
+extract( array_merge( $defaults, $args ) );
+?>
+<div class="container mt-3">
+    <div class="ta-socios-block mx-auto px-3 py-4">
+        <div class="socios-header-container d-flex flex-column">
+            <div class="content">
+                <div class="title">
+                    <h2><?php echo $title; ?></h2>
+                </div>
+                <div class="subtitle">
+                    <p><?php echo $body; ?></p>
+                </div>
+            </div>
+            <div class="img-btn">
+                <div class="img-container text-center mt-3">
+                    <img src="<?php echo TA_THEME_URL; ?>/markup/assets/images/asociate.svg" alt="" class="w-100 h-auto" />
+                </div>
+                <div class="btns-container">
+                    <div class="asociarme-btn d-flex justify-content-center mx-auto mt-3">
+                        <button><?php echo $btn; ?></button>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
