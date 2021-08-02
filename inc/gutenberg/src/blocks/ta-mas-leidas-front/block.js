c045aea6c03c7f7fb74ec5b0046de24bc3efaf3b
Juan Cruz Rabaglia <juanrabaa@gmail.com>
2021-08-02 13:35:31 -0300

    Fixed Más Leídas articles order. Removed some console.logs (#58)

--- inc/gutenberg/src/blocks/ta-mas-leidas-front/block.js
+++ inc/gutenberg/src/blocks/ta-mas-leidas-front/block.js
@@ -7,6 +7,7 @@ const { apiFetch } = wp;
 	if(typeof window.postsBalancer === 'undefined')
 		return;
 
+
 	$(document).ready( async () => {
 		const mostViewedList = document.querySelectorAll(".ta-most-read-articles");
 		const removeMostViewedComponents = () => mostViewedList.forEach( item => $(item).closest('.ta-user-tabs-block').slideUp().remove() );
@@ -16,11 +17,13 @@ const { apiFetch } = wp;
 			return;
 
 		try {
+			let articlesData;
 			let {
 				success: mostViewedFetchSuccess,
 				data: mostViewedArticlesData,
 			} = await fetchLatestMostViewedPosts({
 				show: 5,
+				days_ago: 30,
 			}) ?? {};
 
 			if(!mostViewedFetchSuccess || !mostViewedArticlesData?.length) {
@@ -29,7 +32,21 @@ const { apiFetch } = wp;
 			}
 
 			const mostViewedArticlesIds = mostViewedArticlesData.map( article => article.post_id );
-			console.log('mostViewedArticlesIds', mostViewedArticlesIds);
+
+			/**
+			*	@function fixArticlesOrder
+			*	Puts the articles returned by the Latest Articles API in order by
+			*	amount of views.
+			*/
+			function fixArticlesOrder(){
+				const sortedMostViewedArticles = [];
+				mostViewedArticlesIds.forEach( (articleID) => {
+			    	const article = articlesData.find( article => article.postId == articleID );
+				    if( article )
+				        sortedMostViewedArticles.push(article);
+				});
+				articlesData = sortedMostViewedArticles;
+			}
 
 			// Final arguments for most reads api
 			const articlesArgs = {
@@ -42,8 +59,12 @@ const { apiFetch } = wp;
 				ignore: [],
 			};
 
-			const articlesData = await fetchBalancedArticles({ articlesArgs });
+			articlesData = await fetchBalancedArticles({ articlesArgs });
 
+			if(articlesData?.length)
+				fixArticlesOrder();
+
+			// fixArticlesOrder can change the length of the array it is checked again
 			if(articlesData?.length) {
 				mostViewedList.forEach((mostViewedList, i) => {
 					// Actualizamos los datos (titulo, imagen y link) en los previews placeholder de cada bloque de mas leidas
@@ -62,7 +83,6 @@ const { apiFetch } = wp;
 						}
 					} );
 				});
-
 			}
 			else { // Ocultamos cada bloque de mas leidas
 				removeMostViewedComponents();
