76fc2872a9a61a3bf70df93ae689753ce71cdce6
Juan Iriart <jeiriart@gmail.com>
2021-12-01 15:07:13 -0300

    fix print edition

--- single-ta_ed_impresa.php
+++ single-ta_ed_impresa.php
@@ -4,25 +4,22 @@
 *
 */
 $article = TA_Article_Factory::get_article($post);
+
 ?>
 <?php get_header(); ?>
 
 <?php
-   get_template_part('parts/single', 'article', array(
+get_template_part('parts/single', 'article', array(
     'show_date'             => false,
     'before_social_buttons' => function () use ($article) {
 ?>
-<?php if(is_user_logged_in()): 
-     if (
-        user_active(wp_get_current_user()->ID) && 
-        user_active(wp_get_current_user()->ID) == 'active' &&
-        subscription_user_type(wp_get_current_user()->ID)  === 'digital' ||
-        in_array('administrator', get_user_by('id', wp_get_current_user()->ID)->roles) == 1
-    ) : ?>
-    
-    <a class="ta-ed-impresa-link" href="<?php echo esc_attr($article->issue_pdf['url']); ?>">Ver Online</a>
-<?php  endif; 
-endif; ?>
+    <?php 
+    $userdata = get_userdata(get_current_user_id());
+    $rol = $userdata->roles[0];
+    if (is_user_logged_in() && get_user_meta(get_current_user_id(),'_user_status',true) == 'active' && ($rol == get_option('subscription_digital_role') || $rol == 'administrator')) : ?>
+
+        <a class="ta-ed-impresa-link" href="<?php echo esc_attr($article->issue_pdf['url']); ?>">Ver Online</a>
+    <?php endif; ?>
 <?php
     },
 )); ?>
